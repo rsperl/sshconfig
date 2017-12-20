f41775b1b8dabce7267b5c6918d72d591b504d5e
Richard Sugg <richard.sugg@sas.com>
2017-12-20 10:20:07 -0500

    removed unused code

--- sshconfig/sshconfig.py
+++ sshconfig/sshconfig.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Module Docstring
-"""
-
-__author__ = "Richard Sugg"
-__version__ = "v0.0.0"
-__license__ = "SAS"
-
-import logging
-import argparse
 from io import StringIO
 import re
-import sys
 import os
 
 from datetime import datetime
@@ -71,45 +60,3 @@ class SshConfigEntry(object):
 
     def add_option(self, name, value):
         self.options[name] = value
-
-
-def parseargs():
-    """Parser command line arguments"""
-    parser = argparse.ArgumentParser(description="program description")
-    parser.add_argument("-o", "--option", help="add option")
-    parser.add_argument("-b", "--boolean", action="store_true", help="boolean option")
-    parser.add_argument("-f", "--logfilename", help="logfile name", default=None)
-    parser.add_argument("-n", "--nolog", action="store_true", help="turn off logging", default=False)
-    return parser.parse_args()
-
-
-def init_logging(filename=None):
-    """Initialize logging"""
-    handlers = [logging.StreamHandler()]
-    if filename:
-        handlers.append(logging.FileHandler(filename=filename))
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s %(levelname)s %(module)s %(funcName)s,%(lineno)s %(name)s %(message)s',
-        handlers=handlers
-    )
-
-
-def main():
-    args = parseargs()
-    starttime = datetime.now()
-    print("{}".format(args))
-    if not args.nolog:
-        init_logging(args.logfilename)
-    l = logging.getLogger(__name__)
-    l.info("### starting")
-
-    duration = datetime.now() - starttime
-    l.info("### finished in {}s".format(duration))
-    return 0
-
-
-if __name__ == "__main__":
-    sys.exit(main())
-
-
